18a85a829c792001cfa8f056a35a723d701f70b0
Fatemeh Pakpour <49231214+Fatemeh-Pakpour@users.noreply.github.com>
2019-05-21 09:47:40 +0200

    Replaced  let keyword with const keyword

--- javascript/javascript1/week1/firsthw.js
+++ javascript/javascript1/week1/firsthw.js
@@ -2,19 +2,19 @@
 
 // Age calculator
 
-let yearOfBirth = 1985 ;
-let yearFuture = 2025 ;
-let futureYear =  yearFuture - yearOfBirth ;
+const yearOfBirth = 1985 ;
+const yearFuture = 2025 ;
+const futureYear =  yearFuture - yearOfBirth ;
 console.log (futureYear);
 console.log ("I will be " + futureYear + " years old in " + yearFuture) ;
 
 
 // Goodboy-Oldboy (A dog age calculator)
-let dogYearOfBirth = 2014;
-let dogYearFuture = 2050;
-let ageInHumanYear = dogYearFuture - dogYearOfBirth; 
-let ageOfDog =  ageInHumanYear * 7 ;
-let shouldShowResultInDogYears = true ;
+const dogYearOfBirth = 2014;
+const dogYearFuture = 2050;
+const ageInHumanYear = dogYearFuture - dogYearOfBirth; 
+const ageOfDog =  ageInHumanYear * 7 ;
+const shouldShowResultInDogYears = true ;
 if (shouldShowResultInDogYears) {
     console.log ( " Your dog will be " + ageOfDog +  " dog years old in " +  dogYearFuture  ) ;
   }
@@ -23,26 +23,26 @@ if (shouldShowResultInDogYears) {
   }
 
 // Housey pricey (A house price estimator)
-let width = 8 ;
-let depth = 10 ;
-let height = 10 ;
-let gardenSizeInM2 = 100 ;   //SI derived unit of area with symbol m2
-let volumeInMeters =  width * depth * height ;
-let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300 ;
+const width = 8 ;
+const depth = 10 ;
+const height = 10 ;
+const gardenSizeInM2 = 100 ;   //SI derived unit of area with symbol m2
+const volumeInMeters =  width * depth * height ;
+const housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300 ;
 console.log (housePrice) ;
 if (housePrice < 2500000) {
     console.log ( "Peter and Julia are paying too much")
 }
 
 // Ez Namey (Startup name generator)
-let firstName = ["awesome", "easy", "lovely", "dragon", "serpent", "vegi" ,"cheesy", "coco", "toy", "monster"];
-let secondName = ["clothes", "fifi", "general","basic", "production", "home", "fact", "real", "visual", "world"] ;
+const firstName = ["awesome", "easy", "lovely", "dragon", "serpent", "vegi" ,"cheesy", "coco", "toy", "monster"];
+const secondName = ["clothes", "fifi", "general","basic", "production", "home", "fact", "real", "visual", "world"] ;
 
 const randomNumberFirst = Math.floor ( Math.random() * 10 ) + 0 ;
 console.log (firstName [randomNumberFirst]);
 const randomNumberSecond = Math.floor(Math.random() * 10) + 0 ;
 console.log (secondName[randomNumberSecond]);
-let startupName = firstName[randomNumberFirst] + " " + secondName[randomNumberSecond];
+const startupName = firstName[randomNumberFirst] + " " + secondName[randomNumberSecond];
 console.log ("The startup:" + startupName  + " contains " + startupName.length + " characters") ;
 
 
