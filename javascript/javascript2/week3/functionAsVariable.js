6f0b758e5d679546a00a9201692dafc4dc1bd73b
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-06-24 18:22:45 +0200

    used prettier

--- javascript/javascript2/week3/functionAsVariable.js
+++ javascript/javascript2/week3/functionAsVariable.js
@@ -1,9 +1,6 @@
 const array = [10, 20, 3, 5, 6];
 function randNum() {
-  console.log(
-    "This is function randNum  :",
-    Math.floor(Math.random() * 10)
-  );
+  console.log("This is function randNum:", Math.floor(Math.random() * 10));
 }
 function evenNum() {
   const evenNum = array.filter(num => num % 2 === 0);
@@ -16,20 +13,20 @@ function pushNum() {
 // Create an array with 3 items
 var arrayOfFunction = [randNum, evenNum, pushNum];
 
- while (arrayOfFunction.length) {
-    arrayOfFunction.shift().call();
+while (arrayOfFunction.length) {
+  arrayOfFunction.shift().call();
 }
-// Create a function as a const 
+// Create a function as a const
 
 const oddNumber = function() {
-    const oddNum = array.filter(num => num % 2 !== 0);
-    console.log("this is function oddNumber :", oddNum);
-  }
+  const oddNum = array.filter(num => num % 2 !== 0);
+  console.log("this is function oddNumber :", oddNum);
+};
 oddNumber();
 
 // Create an object that has a key whose value is a function
 obj = {
-    arr : oddNumber,
-    name:"odd number"
-}
- obj.arr();
+  arr: oddNumber,
+  name: "odd number"
+};
+obj.arr();
