b80e3ce5fa9147e135b598317c9176fecff9b141
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-07-15 17:14:00 +0200

    Added geolocation assignment

--- javascript/javascript3/week2/afterSetTime/main.js
+++ javascript/javascript3/week2/afterSetTime/main.js
@@ -1,9 +1,21 @@
+// variables
+const locationButton = document.querySelector(".btn_location");
+const messageDisply = document.querySelector("#message");
 const status = true;
+
+// event
+locationButton.addEventListener("click", () => {
+  afterSetTime(3000).catch(err => console.log(err));
+  getCurrentLocation();
+});
+
 function afterSetTime(resolveAfter) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (status) {
-        return resolve(displyMessage());
+        return resolve(
+          (messageDisply.textContent = "I am called asynchronously")
+        );
       } else {
         return reject(Error("There is a problem"));
       }
@@ -11,10 +23,32 @@ function afterSetTime(resolveAfter) {
   });
 }
 
-function displyMessage() {
-  document.querySelector("#message").textContent = "I am called asynchronously";
+// get geolocation
+function getCurrentLocation() {
+  const promise1 = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const currentLocation = {
+        longitude: position.coords.longitude,
+        latitude: position.coords.latitude
+      };
+      resolve(
+        renderLocationOnGoogleMap(
+          currentLocation.latitude,
+          currentLocation.longitude
+        )
+      );
+      reject(Error("fail"));
+    });
+  });
+  return promise1
+    .then(console.log("Map is rendered"))
+    .catch(err => consoel.log(err));
 }
 
-document.querySelector("#btn_click").addEventListener("click", () => {
-  afterSetTime(3000).catch(err => console.log(err));
-});
+function renderLocationOnGoogleMap(lat, lng) {
+  const mapDiv = document.querySelector("#map");
+  const map = new google.maps.Map(mapDiv, {
+    center: { lat, lng },
+    zoom: 12
+  });
+}
